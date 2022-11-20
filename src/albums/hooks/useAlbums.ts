30a63550e4a181df0d306db5c7dcea7ee35e3a07
Olena <elena@thecarrot.space>
2022-11-20 21:13:30 +0100

    Calculate the dominant color for every album

--- src/albums/hooks/useAlbums.ts
+++ src/albums/hooks/useAlbums.ts
@@ -1,5 +1,7 @@
 import { useState } from 'react';
-import dataProvider, { RawAlbumData } from './dataProvider';
+import dataProvider, { RawAlbumData } from '../data/dataProvider';
+import { getPredominantColorFromImgURL } from 'color-thief-react/lib/utils';
+import { HSBToRGB, RGBToHSB } from '../../utils/colors';
 
 export default () => {
   const [loading, setLoading] = useState<boolean>(false);
@@ -10,11 +12,12 @@ export default () => {
 
     try {
       const response = await dataProvider.getAlbums();
-      setData(() =>
+      const albums = await Promise.all(
         response.feed.entry.map((rawEntry, index: number) =>
           dataMapper(rawEntry, index + 1)
         )
       );
+      setData(() => albums);
     } finally {
       setLoading(false);
     }
@@ -23,14 +26,28 @@ export default () => {
   return { fetchAlbums, loading, data };
 };
 
-const dataMapper = (rawData: RawAlbumData, ranking: number): Album => {
+const normalizeDominantColor = ([r, g, b]: [number, number, number]): [
+  number,
+  number,
+  number
+] => {
+  const [h] = RGBToHSB(r, g, b);
+  return HSBToRGB(h, 100, 50);
+};
+
+const dataMapper = async (
+  rawData: RawAlbumData,
+  ranking: number
+): Promise<Album> => {
   const rawImageData = rawData['im:image'][rawData['im:image'].length - 1];
   return {
     id: rawData.id.attributes['im:id'],
     name: rawData['im:name'].label,
     link: rawData.link.attributes.href,
+    itemCount: rawData['im:itemCount'].label,
     image: {
-      src: rawImageData.label,
+      // workaround for better image quality
+      src: rawImageData.label.replace('170x170bb.png', '632x632bb.webp'),
       attributes: rawImageData.attributes,
     },
     category: {
@@ -41,7 +58,15 @@ const dataMapper = (rawData: RawAlbumData, ranking: number): Album => {
     artist: {
       name: rawData['im:artist'].label,
     },
+    rights: rawData.rights.label,
     ranking,
+    dominantColor: normalizeDominantColor(
+      await getPredominantColorFromImgURL(
+        rawImageData.label,
+        'rgbArray',
+        'anonymous'
+      )
+    ),
   };
 };
 
@@ -49,6 +74,7 @@ export interface Album {
   id: string;
   name: string;
   link: string;
+  itemCount: string;
   image: {
     src: string;
     attributes: {
@@ -64,4 +90,6 @@ export interface Album {
     name: string;
   };
   ranking: number;
+  dominantColor: [number, number, number];
+  rights: string;
 }
